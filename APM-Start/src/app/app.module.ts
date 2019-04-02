e556f8094b170885ff235e0b95f5ed2382ce6e0b
fabrizioiacovone <ing.fabrizioiacovone@gmail.com>
2019-04-02 10:29:36 +0200

    Angular-GetStarted-Initial commit ACME products management

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces-pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
