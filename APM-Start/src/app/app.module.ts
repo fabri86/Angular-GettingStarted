1fa1f84dcfda875ce35a099afe7a6af8dc1fe3df
Fabri <fabrizio.iacovone@avanade.com>
2019-04-03 10:37:36 +0200

    Added product module and refactored the app accordingly

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,37 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces-pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailsComponent } from './products/product-details.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ProductDetailsComponent,
-    WelcomeComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailsComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-    { path: 'products', component: ProductListComponent },
-    { path: 'products/:id', component: ProductDetailsComponent },
-    { path: 'welcome', component: WelcomeComponent },
-    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-    { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
-  ])
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+    ]),
+    ProductModule
 ],
   bootstrap: [AppComponent]
 })
--- APM-Start/src/app/products/product-details.component.ts
+++ APM-Start/src/app/products/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-details.component.html',
@@ -9,9 +10,27 @@ export class ProductDetailsComponent implements OnInit {
   pageTitle: string = 'Product Details';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.product =
+      {
+        'productId': 10,
+        'productName': 'Video Game Controller',
+        'productCode': 'GMG-0042',
+        'releaseDate': 'October 15, 2015',
+        'description': 'Standard two-button video game controller',
+        'price' : 35.95,
+        'starRating' : 4.6,
+        'imageUrl' : 'https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png'
+      };
+
+      this.pageTitle += `: ${id} `;
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
+
